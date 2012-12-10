937468132aba27bb8e6bae122b59b9d66c66152b
arnose <arnose@gmail.com>
2012-12-10 14:02:47 +0800

    Fix some minor formats and using prefix to store directory information

--- task_db.py
+++ task_db.py
@@ -4,8 +4,9 @@ import re
 import date_op
 
 class task_db:
-    todo_file = './todo.txt'
-    done_file = './done.txt'
+    prefix = '/home/arnose/Dropbox/todotxt'
+    todo_file = prefix+'/todo.txt'
+    done_file = prefix+'/done.txt'
     digits = 2
 
     def __init__(self, conf=None):
@@ -68,7 +69,7 @@ class task_db:
         f = open(self.todo_file, 'a')
         f.write(task+'\n')
         f.close()
-        # Added task needs to be updated in todos
+
         task_prop = self.__parse_task(task)
         tid = len(self.todos['list'])
         for prj in task_prop['prjs']:
@@ -104,8 +105,8 @@ class task_db:
         due_pattern = self.get_pattern('due')
         con_pattern = self.get_pattern('con')
         pri_pattern = self.get_pattern('pri')
-        prjs = prj_pattern.findall(task) # Use sets for storing projects 
-        dues = due_pattern.findall(task) # Array
+        prjs = prj_pattern.findall(task)
+        dues = due_pattern.findall(task)
         cons = con_pattern.findall(task)
         pris = pri_pattern.findall(task)
 
@@ -119,6 +120,7 @@ class task_db:
         due_dict = dict()
         con_dict = dict()
         pri_dict = dict()
+
         for i, line in enumerate(lines):
             task_prop = self.__parse_task(line)
             for prj in task_prop['prjs']:
